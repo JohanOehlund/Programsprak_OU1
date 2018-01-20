1bd23f296d3ab956f0447b16c25c7e1965a820a4
c15jodHome <joah0087@student.umu.se>
2018-01-20 22:19:39 +0100

    gg

--- phus.hs
+++ phus.hs
@@ -30,13 +30,55 @@ phus [] = []
 --phus l =  phus_helper l []
 
 
+
+
 phus_helper :: [(String, Bool, (Integer, Integer))] -> [(String, Bool, (Integer, Integer),(Integer, Integer))] -> 
     [(String, Bool, (Integer, Integer),(Integer, Integer))]
 phus_helper [] output = output
 phus_helper ((reg0,isParked0,(hour0,minute0)):xs) output
-    | ((elem') output reg0) == True = phus_helper xs output
-    | otherwise = phus_helper xs (((reg0,isParked0,(0,0),(hour0,minute0))) : output)
+    | ((elem') output reg0) == False = phus_helper xs (((reg0,isParked0,(0,0),(hour0,minute0))) : output)
+    | isParked0 == True =  phus_helper xs output
+    | isParked0 == False = phus_helper xs output
+    | otherwise = []
 
+
+
+test :: [(String, Bool, (Integer, Integer))] -> (String, Bool, (Integer, Integer),(Integer, Integer)) -> 
+    (String, Bool, (Integer, Integer),(Integer, Integer))
+test [] output =output
+test ((reg0,isParked0,(hour0,minute0)):xs) (reg1,isParked1,(cHour,cMinute),(hour1,minute1))
+    | reg0 == reg1 && isParked1==True && isParked0 == False = test xs (reg1,isParked0,(diffTime (cHour,cMinute) (hour1,minute1) (hour0,minute0)),(hour0,minute0))
+    | reg0 == reg1 && isParked1==False && isParked0 ==True = test xs (reg1,isParked0,(cHour,cMinute),(hour0,minute0))
+    |otherwise = test xs (reg1,isParked1,(cHour,cMinute),(hour1,minute1))
+
+diffTime :: (Integer,Integer) -> (Integer,Integer) -> (Integer,Integer)-> (Integer,Integer)
+diffTime (currHour,currMinute) (checkInHour,checkInMinute) (checkOutHour,checkOutMinute) 
+    | checkInMinute == checkOutMinute = (currHour+((abs)checkOutHour-checkInHour)-1,(currMinute+((abs)(((checkOutHour*60)+checkOutMinute)-((checkInHour*60)+checkInMinute)) `mod` 60)))
+    | checkInMinute+checkOutMinute >=60 = (currHour+((abs)(((checkOutHour*60)+checkOutMinute)-((checkInHour*60)+checkInMinute)) `mod` 24),(currMinute+((abs)(((checkOutHour*60)+checkOutMinute)-((checkInHour*60)+checkInMinute)) `mod` 60)))
+    | otherwise= (currHour+(((abs)checkOutHour-checkInHour)-1),(currMinute+((abs)(((checkOutHour*60)+checkOutMinute)-((checkInHour*60)+checkInMinute)) `mod` 60)))
+{-
+
+diffTime :: (Integer,Integer) -> (Integer,Integer) -> (Integer,Integer)-> (Integer,Integer)
+diffTime (currHour,currMinute) (checkInHour,checkInMinute) (checkOutHour,checkOutMinute) 
+    | checkInMinute+checkOutMinute >= 60 && checkOutHour-checkInHour < 24 = ((currHour+abs(checkOutHour-checkInHour)-1),(currMinute+ abs((checkOutMinute+checkInMinute)-60)))
+    | otherwise = ((currHour+abs(checkOutHour-checkInHour)),(currMinute+abs(checkOutMinute-checkInMinute)))
+
+checkIn :: (String,(Integer,Integer)) -> [(String, Bool, (Integer, Integer),(Integer, Integer))] ->
+    (String, Bool, (Integer, Integer),(Integer, Integer))
+checkIn (reg0,(hour0,minute0)) ((reg1,isParked1,(cHour,cMinute),(hour1,minute1)):xs)
+    | reg0 ==reg1 = (reg1,isParked1,(cHour+hour0,cMinute+minute0),(hour1,minute1))
+    | otherwise = checkIn (reg0,(hour0,minute0)) xs
+
+split :: [(String,Bool,(Integer,Integer))] -> ([(String,Bool)], [(Integer,Integer)])
+split [] = ([], [])
+split [(reg,isParked,(hour,minute))] = ([(reg,isParked)], [(hour,minute)])
+addTime :: (Eq a, Num t3, Num t2, Num t1, Num t) => [(a, Bool, (t, t1), (t2, t3))] -> (a, Bool, (t, t1)) ->
+    [(a, Bool, (t, t1), (t2, t3))]
+addTime [] (reg0,isParked0,(hour0,minute0)) = []
+addTime ((reg1,isParked1,(cHour,cMinute),(_,_)):xs) (reg0,isParked0,(hour0,minute0))
+    | reg1 == reg0 = (reg1,isParked1,((cHour+hour0),(cMinute+minute0)),(hour0,minute0))
+    | otherwise = addTime xs (reg0,isParked0,(hour0,minute0))
+-}
 
 
 {-
