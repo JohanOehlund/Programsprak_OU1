fbe9e192366221e24443957bfbe3f714c8fe7456
c15jodHome <joah0087@student.umu.se>
2018-01-24 01:27:24 +0100

    KLART GG

--- phus.hs
+++ phus.hs
@@ -9,55 +9,37 @@ Comment: Calculates total time parked for each car in the carpark.
 -}
 phus :: CarparkInfoList ->  CarparkOutputTuple
 phus l = convertToOutput calcDayX maxParked []
-        where calcDayX = (map') l  $phus_helper l []
-              maxParked = getMaxTimeParked calcDayX ("ABC123",(0,0))
+       where calcDayX = (map') l 
+             maxParked = getMaxTimeParked calcDayX ("ABC123",(0,0))
 
 {-
-Function: phus_helper
-Comment: Checks if a car is an element of a list.
+Function: notElem'
+Comment: Checks if a car is not an element of a list.
 -}
-phus_helper :: CarparkInfoList -> [(String, Bool, TimeParked,TimeParked)] -> 
-    [(String, Bool, TimeParked,TimeParked)]
-phus_helper [] output = output
-phus_helper ((reg0,isParked0,(hour0,minute0)):xs) output
-    | ((elem') output reg0) == False = phus_helper xs (((reg0,isParked0,(0,0),(hour0,minute0))) : output)
-    | otherwise = phus_helper xs output
-
-
-{-
-Function: elem'
-Comment: Checks if a car is an element of a list.
--}
-elem' :: (Eq a, Num t3, Num t2, Num t1, Num t) => [(a, Bool, (t, t1), (t2, t3))] -> a -> Bool
-elem' [] reg1 = False
-elem' ((reg0,_,(_,_),(_,_)):xs) reg1
-    | reg0 == reg1 = True
-    | otherwise = (elem') xs reg1   
-
+notElem' :: (Eq a, Num t1, Num t) => (a,Bool,(t, t1)) -> a -> Bool
+notElem' (reg0,_,(_,_)) reg1
+    | reg0 /= reg1 = True 
+    | otherwise = False
 
 {-
 Function: map'
 Comment: converted map function inorder to apply every calcTimeDiffs function to each element of the list from
 phus_helper.
 -}
-map' :: CarparkInfoList -> [(String, Bool, TimeParked,TimeParked)] -> 
-    [(String, Bool, TimeParked,TimeParked)]
-map' _ [] = []
-map' dayX ((reg1,isParked1,(cHour,cMinute),(hour1,minute1)):xs) = calcTimeDiffs dayX (reg1,isParked1,(cHour,cMinute),(hour1,minute1)) : map' dayX xs
-
-
+map' :: CarparkInfoList -> [(String,TimeParked)]
+map' [] = []
+map' ((reg,isParked,(hour,minute)):xs) = (calcTimeDiffs ((reg,isParked,(hour,minute)):xs) (reg,isParked,(hour,minute),(0,0))) : map' [x|x <- xs ,(notElem') x reg] 
 
 {-
 Function: calcTimeDiffs
 Comment: Calculates the total time parked for each car in the carpark.
 -}
-calcTimeDiffs :: CarparkInfoList -> (String, Bool, TimeParked,TimeParked) -> 
-    (String, Bool, TimeParked,TimeParked)
-calcTimeDiffs [] output =output
-calcTimeDiffs ((reg0,isParked0,(hour0,minute0)):xs) (reg1,isParked1,(cHour,cMinute),(hour1,minute1))
-    | reg0 == reg1 && isParked1==True && isParked0 == False = calcTimeDiffs xs (reg1,isParked0,(diffTime (cHour,cMinute) (hour1,minute1) (hour0,minute0)),(hour0,minute0))
-    | reg0 == reg1 && isParked1==False && isParked0 ==True = calcTimeDiffs xs (reg1,isParked0,(cHour,cMinute),(hour0,minute0))
-    | otherwise = calcTimeDiffs xs (reg1,isParked1,(cHour,cMinute),(hour1,minute1))
+calcTimeDiffs :: CarparkInfoList -> (String,Bool,TimeParked,TimeParked) -> (String,TimeParked)
+calcTimeDiffs [] (reg,_,(hour,minute),(totHour,totMinute)) =(reg,(totHour,totMinute))
+calcTimeDiffs ((reg0,isParked0,(hour0,minute0)):xs) (reg1,isParked1,(hour1,minute1),(totHour,totMinute)) 
+    | reg0 == reg1 && isParked1==True && isParked0 == False = calcTimeDiffs xs (reg1,isParked0,(hour0,minute0),(diffTime (totHour,totMinute) (hour1,minute1) (hour0,minute0)))
+    | reg0 == reg1 && isParked1==False && isParked0 ==True = calcTimeDiffs xs (reg1,isParked0,(hour0,minute0),(totHour,totMinute))
+    | otherwise = calcTimeDiffs xs (reg1,isParked1,(hour1,minute1),(totHour,totMinute))
 
 {-
 Function: diffTime
@@ -71,17 +53,17 @@ diffTime (currHour,currMinute) (checkInHour,checkInMinute) (checkOutHour,checkOu
 Function: convertToOutput
 Comment: Converts the output from getMaxTimeParked and map' inorder to match the expected output of the function phus.
 -}
-convertToOutput :: [(String, Bool, TimeParked,TimeParked)] -> String -> [(String, TimeParked)]  -> CarparkOutputTuple
+convertToOutput :: [(String,TimeParked)] -> String -> [(String, TimeParked)]  -> CarparkOutputTuple
 convertToOutput [] reg output = (reg,output)
-convertToOutput ((reg1,isParked1,(cHour,cMinute),(hour1,minute1)):xs1) reg output =  convertToOutput xs1 reg ((reg1,(cHour,cMinute)) : output) 
+convertToOutput ((reg1,(totHour,totMinute)):xs1) reg output =  convertToOutput xs1 reg ((reg1,(totHour,totMinute)) : output) 
 
 {-
 Function: getMaxTimeParked
 Comment: Returns the car with the maximum time parked in the carpark.
 -}
-getMaxTimeParked :: [(String, Bool, TimeParked,TimeParked)] -> (String,TimeParked) -> String
+getMaxTimeParked :: [(String,TimeParked)] -> (String,TimeParked) -> String
 getMaxTimeParked [] (reg,(hour,minute)) = reg 
-getMaxTimeParked ((reg1,isParked1,(cHour,cMinute),(hour1,minute1)):xs) (reg,(hour,minute)) = if ((cHour*60)+cMinute) > ((hour*60)+minute) then
-                        getMaxTimeParked xs (reg1,(cHour,cMinute)) 
+getMaxTimeParked ((reg1,(totHour,totMinute)):xs) (reg,(hour,minute)) = if ((totHour*60)+totMinute) > ((hour*60)+minute) then
+                        getMaxTimeParked xs (reg1,(totHour,totMinute)) 
                      else 
                         getMaxTimeParked xs (reg,(hour,minute))
